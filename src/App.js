33e70dc5d626f257b686a957ef78655d3a5208b3
syedhaseeb1 <haseebumerzai@gmail.com>
2023-03-23 22:19:09 +0500

    Video 11

--- src/App.js
+++ src/App.js
@@ -2,17 +2,13 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Textform from "./components/Textform";
 function App() {
-  let name = "Kabootar";
-  let desc = "A practice website for learning";
-  let reactJS = "React Js Framework";
-
   return (
     <>
       <Navbar title="TextUtils" />
       <div className="container my-3">
-      <Textform title="Enter your text"/>
+        <Textform title="Enter your text"/>
+        {/* <About /> */}
       </div>
-      
     </>
   );
 }
